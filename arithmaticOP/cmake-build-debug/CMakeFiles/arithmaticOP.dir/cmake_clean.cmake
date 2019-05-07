bfb0a0a9fe82070ec21f6be871514b521b90b47c
brax3894 <42273897+brax3894@users.noreply.github.com>
2019-05-07 18:51:06 -0400

    basic math

---
+++ arithmaticOP/cmake-build-debug/CMakeFiles/arithmaticOP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arithmaticOP.dir/main.cpp.obj"
+  "arithmaticOP.pdb"
+  "arithmaticOP.exe"
+  "arithmaticOP.exe.manifest"
+  "libarithmaticOP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arithmaticOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ arithmaticOP/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1 (200);
+    int num2 (100);
+    cout << num1 <<"+" << num2 << "=" << num1 + num2 << endl;
+
+    int result (0);
+
+    result = num1+num2;
+
+    cout << num1 << "+" << num2 <<"="<<  result << endl;
+
+    result = num1+num2;
+
+    cout << num1 << "+" << num2 <<"="<<  result << endl;
+    result = num1+num2;
+
+    cout << num1 << "+" << num2 <<"="<<  result << endl;
+    return 0;
+}
