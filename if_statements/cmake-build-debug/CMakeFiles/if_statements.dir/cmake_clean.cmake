91baee568d596fcb3d253276e0271b4b840c60d9
brax3894 <42273897+brax3894@users.noreply.github.com>
2019-05-07 20:03:35 -0400

    ifs and a re-upload

---
+++ if_statements/cmake-build-debug/CMakeFiles/if_statements.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/if_statements.dir/main.cpp.obj"
+  "if_statements.pdb"
+  "if_statements.exe"
+  "if_statements.exe.manifest"
+  "libif_statements.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/if_statements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
