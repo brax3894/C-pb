1ffd7ca6b93df17e31d851e894b4813df9aa4468
brax3894 <42273897+brax3894@users.noreply.github.com>
2019-05-22 14:04:02 -0400

    define functions__Area and volume of a circle

---
+++ define_functions_Area_Volume/cmake-build-debug/CMakeFiles/define_functions_Area_Volume.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/define_functions_Area_Volume.dir/main.cpp.obj"
+  "define_functions_Area_Volume.pdb"
+  "define_functions_Area_Volume.exe"
+  "define_functions_Area_Volume.exe.manifest"
+  "libdefine_functions_Area_Volume.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/define_functions_Area_Volume.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
