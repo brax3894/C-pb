fc8cceee9645233e9d8f4d0bf18e3b410a3398c3
brax3894 <42273897+brax3894@users.noreply.github.com>
2019-05-30 11:05:12 -0400

    deference pointers

---
+++ pointers_dereference/cmake-build-debug/CMakeFiles/pointers_dereference.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointers_dereference.dir/main.cpp.obj"
+  "pointers_dereference.pdb"
+  "pointers_dereference.exe"
+  "pointers_dereference.exe.manifest"
+  "libpointers_dereference.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointers_dereference.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
